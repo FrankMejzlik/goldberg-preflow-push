e6330c8f9771153baea80baee72a7dbeac750800
Frank Mejzlik <frankmejzlik@gmail.com>
2020-06-07 12:29:12 +0200

    Add linter and hindent. Initialization is done.

--- main.hs
+++ main.hs
@@ -1,4 +1,3 @@
-
 {-
 The main source file for highest-label push-relabel (Goldberg) algorithm.
 
@@ -13,96 +12,95 @@ DATA STRUCTURES:
     FlowNet - Flow network (G, s, t, c) where we use G as list of neighbours where each edge also holds capacity ().
 
 -}
-
-
 import Types
 
 -- stack ghci --package pretty-simple ./main.hs
 import Text.Pretty.Simple
 
-
--- Test network #1
-net1_V = [V 0, V 1, V 2, V 3]
-net1_E = [
-        E   0 1   2.0, 
-        E   0 2   4.0, 
-
-        E   1 2   3.0, 
-
-        E   2 3   5.0, 
-        E   1 3   1.0
-    ]
-
 -- Checkpoints
 net1 = N net1_V net1_E 0 3
-net1_postInitialize = push (toEdgetendedNetwork net1) 
+
+net1_postInitialize = push (toEdgetendedNetwork net1)
 
 goldInitialze net@(N vs es s t) = foldr (flip push) (toEdgetendedNetwork net) (getOutEdges es s)
 
 getOutEdges :: [E] -> Int -> [(VertId, VertId)]
-getOutEdges es from =  map (\(E fr to _ ) -> (fr, to))  (filter (\(E fr to _ ) -> fr == from) es)
-
+getOutEdges es from = map (\(E fr to _) -> (fr, to)) (filter (\(E fr to _) -> fr == from) es)
 
 {- Converts user network to representation for the algorithm calculations -}
 toEdgetendedNetwork :: N -> Network
-toEdgetendedNetwork (N vs es s t) = Network vss ess s t (take ((length vs) + 1) (repeat []))
-    where 
-        vss = map fv vs     -- Edgetended vertices
-        ess = map fe es     -- Edgetended edges
+toEdgetendedNetwork (N vs es s t) = Network vss ess s t (replicate (length vs + 1) [])
+  where
+    vss = map fv vs -- Edgetended vertices
+    ess = map fe es -- Edgetended edges
+    fv v@(V name)
+        | name == s = Vertex name (length vs) capSum (map (\(E fr to c) -> (fr, to)) es)
+        | name == t = Vertex name 0 0.0 []
+        | otherwise = Vertex name 0 0.0 []
+    fe (E fr to c) = Edge fr to c 0.0
+    capSum = sum (map (\(E _ _ c) -> c) es) -- Sum of all edge capacities
 
-        fv v@(V name)
-            | name == s    = Vertex name (length vs) capSum (map (\(E fr to c) -> (fr, to)) es)
-            | name == t    = Vertex name 0 0.0 []
-            | otherwise = Vertex name 0 0.0 []
-
-        fe (E fr to c) = Edge fr to c 0.0             
-
-        capSum = sum (map (\(E _ _ c) -> c) es) -- Sum of all edge capacities
-
-{- Gets only normal vertices from the network -}    
-normVertices (Network vs es s t _) = filter (\ (Vertex n h ex des) -> n /= s && n /= t) vs
+{- Gets only normal vertices from the network -}
+normVertices (Network vs es s t _) = filter (\(Vertex n h ex des) -> n /= s && n /= t) vs
 
 {- Pushes possible maximum through the provided edge -}
 push :: Network -> (VertId, VertId) -> Network
 push net@(Network vs es s t netQ) (fr, to) = pushedNet
-    where
-        pushedNet = Network (newFromVert : newToVert : restVerts) (newEdge : restEdges) s t newQ
-
-        -- Amount to push
-        diff = min fromEx edgeRes
-
+  where
+    pushedNet = Network (newFromVert : newToVert : restVerts) (newEdge : restEdges) s t newQ
+        -- Amount to push 
+    diff = min fromEx edgeRes
         -- Handle queue properly
-        newQ = updateQueue netQ diff fromVert toVert
-
-        -- Old vertices
-        fromVert@(Vertex frName frH fromEx frDes) = findVertex vs fr
-        toVert@(Vertex toName toH toEx toDes) = findVertex vs to
-        restVerts = filter (\(Vertex name _ _ _) -> name /= fr && name /= to) vs
-
-        -- New vertices
-        newFromVert = Vertex frName frH (fromEx - diff) frDes
-        newToVert = Vertex toName toH (toEx + diff) toDes
-
-        -- Old edges
-        edgeRes = eCap - eFlow
-        foundEdge@(Edge eFr eTo eCap eFlow) = findEdge es fr to
-        restEdges = findEdgeComplement es fr to
-
-        -- New edges
-        newEdge = Edge eFr eTo eCap (eFlow + diff)
+    newQ = updateQueue netQ diff fromVert toVert
+    -- Old vertices
+    fromVert@(Vertex frId frH fromEx frDes) = findVertex vs fr
+    toVert@(Vertex toId toH toEx toDes) = findVertex vs to
+    restVerts = filter (\(Vertex name _ _ _) -> name /= fr && name /= to) vs
+    -- New vertices 
+    newFromVert = Vertex frId frH (fromEx - diff) frDes
+    newToVert = Vertex toId toH (toEx + diff) toDes
+    -- Old edges
+    edgeRes = eCap - eFlow
+    foundEdge@(Edge eFr eTo eCap eFlow) = findEdge es fr to
+    restEdges = findEdgeComplement es fr to
+    -- New edges
+    newEdge = Edge eFr eTo eCap (eFlow + diff)
 
 {- Updates the bucket queue after the push based on provided arguments. -}
 updateQueue :: [[VertId]] -> Double -> Vertex -> Vertex -> [[VertId]]
-updateQueue netQ diff (Vertex frName frH frEx frDes) (Vertex toName toH toEx toDes) = netQ
+updateQueue netQ diff (Vertex frId frH frEx frDes) (Vertex toId toH toEx toDes) = newNetQ
+  where
+    cleanQueue = getCleanQueue netQ frId toId
+    newNetQ = cleanQueue
+
+{- Removes from and to vertices from active vertex queue. -}
+getCleanQueue q frId toId = map (filter (\vId -> vId /= frId && vId /= toId)) q
 
 {-- Gets edge with specified name -}
 findVertex vs tarName = head $ filter (\(Vertex name _ _ _) -> name == tarName) vs
 
 {-- Gets edge with specified from/to values -}
 findEdge es fr to = head $ filter (\(Edge _fr _to _ _) -> _fr == fr && _to == to) es
+
 findEdgeComplement es fr to = filter (\(Edge _fr _to _ _) -> _fr /= fr || _to /= to) es
 
-myLen [] = 0
-myLen (x : xs) = myLen xs + 1
 
-myLen2 xs = foldr (\x acc -> acc + 1) 0 xs
+{--------------------------------------------
+ TESTS
+---------------------------------------------}
+-- Test network #1
+net1_V = [V 0, V 1, V 2, V 3]
+
+net1_E = [E 0 1 2.0, E 0 2 4.0, E 1 2 3.0, E 2 3 5.0, E 1 3 1.0]
+
+-- Test Queue
+t_q1 = [[1, 4, 5, 6], [], [3, 2]]
+
+{- TEST: `getCleanQueue` -}
+test_getCleanQueue = [test1_q1, test2_q1, test3_q1]
+
+test1_q1 = getCleanQueue t_q1 3 1 == [[4, 5, 6], [], [2]]
+
+test2_q1 = getCleanQueue t_q1 6 2 == [[1, 4, 5], [], [3]]
+
+test3_q1 = getCleanQueue t_q1 33 33 == [[1, 4, 5, 6], [], [3, 2]]
--- types.hs
+++ types.hs
@@ -4,19 +4,25 @@ module Types where
     User input data types 
 -}
 -- Vertex
-data V = 
-    V { id_::Int }   -- Vertex <ID>
-        deriving (Eq, Show)
+data V =
+    V
+        { id_ :: Int 
+        } -- Vertex <ID>
+    deriving (Eq, Show)
 
 -- Edge
-data E = 
-    E { fr_::Int, to_::Int, c_::Double }   -- Edge { <from_ID>, <to_ID>, <capacity> }
-        deriving (Eq, Show)
+data E =
+    E
+        { fr_ :: Int
+        , to_ :: Int
+        , c_ :: Double
+        } -- Edge { <from_ID>, <to_ID>, <capacity> }
+    deriving (Eq, Show)
 
 -- Network
-data N = 
-    N [V] [E] Int Int         -- FlowNetwork { <vertices>, <edges> }
-        deriving (Eq, Show)
+data N =
+    N [V] [E] Int Int -- FlowNetwork { <vertices>, <edges> }
+    deriving (Eq, Show)
 
 {- 
     Algorithm meta-types used 
@@ -25,17 +31,32 @@ type VertId = Int
 
 -- Vertex holding extra list of descending edges
 -- (Vertex name h ex des)
-data Vertex = 
-    Vertex  { vertex_name::VertId,     vertex_h::Int, vertex_ex::Double, vertex_des::[(VertId, VertId)] }
-        deriving (Eq, Show)
+data Vertex =
+    Vertex
+        { vertex_name :: VertId
+        , vertex_h :: Int
+        , vertex_ex :: Double
+        , vertex_des :: [(VertId, VertId)]
+        }
+    deriving (Eq, Show)
 
 -- Edgetended edge holding flow going through it
-data Edge = 
-    Edge { edge_fr::Int, edge_to::Int, eedge_c::Double, edge_f::Double } 
-        deriving (Eq, Show)
+data Edge =
+    Edge
+        { edge_fr :: Int
+        , edge_to :: Int
+        , eedge_c :: Double
+        , edge_f :: Double
+        }
+    deriving (Eq, Show)
 
 -- Network structure for algorithm computation with extra meta data
-data Network = 
-    Network { network_vs::[Vertex],  network_es::[Edge], network_s::Int, network_t::Int, network_Q::[[VertId]] }
-        deriving (Eq, Show)
-
+data Network =
+    Network
+        { network_vs :: [Vertex]
+        , network_es :: [Edge]
+        , network_s :: Int
+        , network_t :: Int
+        , network_Q :: [[VertId]]
+        }
+    deriving (Eq, Show)
